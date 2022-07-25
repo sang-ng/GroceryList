fbb54bd70dd75ca57e6cd5cae78d38d0c9233a12
sang-ng <sanguyen300@gmail.com>
2022-07-25 08:22:22 +0200

    added ShoppingITemDao

--- app/src/main/java/com/sanguyen/android/grocerylist/feature_shoppingitem/data/data_source/ShoppingItemDao.kt
+++ app/src/main/java/com/sanguyen/android/grocerylist/feature_shoppingitem/data/data_source/ShoppingItemDao.kt
@@ -1,12 +1,21 @@
 package com.sanguyen.android.grocerylist.feature_shoppingitem.data.data_source
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
+import com.sanguyen.android.grocerylist.feature_shoppingitem.domain.model.ShoppingItem
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ShoppingItemDao {
 
-    @Query("SELECT * FROM")
-    fun getShoppingItems() : Flow<List<ShoppingItem>>
+    @Query("SELECT * FROM ShoppingItem")
+    fun getShoppingItems(): Flow<List<ShoppingItem>>
+
+    @Query("SELECT * FROM ShoppingItem WHERE id = :id")
+    suspend fun getNoteById(id: Int): ShoppingItem?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+    @Delete
+    suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
 }
